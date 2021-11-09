93b03faf3b178071f5f7e96ba639b5b9872d56f1
nastre3 <nastre70@gmail.com>
2021-11-09 22:32:43 +1000

    Added direction, y (task 22.1.2)

--- Cat/src/Main.java
+++ Cat/src/Main.java
@@ -8,8 +8,10 @@ which can move forward and backward
         Tank tank = new Tank();
         tank.goForward(10);
         tank.printPosition();
+        tank.turnRight();
         tank.goForward(50);
         tank.printPosition();
+        tank.turnLeft();
         tank.goBackward(100);
         tank.printPosition();
 
--- Cat/src/Tank.java
+++ Cat/src/Tank.java
@@ -1,24 +1,41 @@
 public class Tank {
     int x = 0;
+    int y = 0;
+    int dir;
     
     public Tank() {
-        this(0);
+        this(0, 0);
     }
-    
-    public Tank(int x) {
+
+    public Tank(int x, int y) {
         this.x = x;
+        this.y = y;
     }
 
-
+    //0 - вправо, 1  - вниз, 2  - влево, 3 - вверх
     public void goForward(int x) {
-        this.x += x;
+        if (dir==0) this.x += x;
+        else if (dir == 1) y+=x;
+        else if (dir == 2) this.x -= x;
+        else y -=x;
     }
 
     public void goBackward(int x) {
-        this.x -= x;
+        goForward(-x);
     }
 
     public void printPosition() {
-        System.out.printf("The Tank is at %d now.\n", x);
+        System.out.printf("The Tank is at %d, %d now.\n", x, y);
+        System.out.println();
+    }
+
+    public void turnRight() {
+        dir++;
+        if (dir == 4) dir = 0;
+    }
+
+    public void turnLeft() {
+        dir--;
+        if (dir == -1) dir = 3;
     }
 }
