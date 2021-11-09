d7b72a0fe953ed260cea5ed75c4d53b22e5fc05a
nastre3 <nastre70@gmail.com>
2021-11-09 23:05:22 +1000

    Added fuel, not finished task 22.1.3

--- Cat/src/Main.java
+++ Cat/src/Main.java
@@ -1,26 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-Make a Tank(add code to class Tank),
-which can move forward and backward
+/*
+Make (add code to class Tank),
+with fuel and different constructors
 */
 
-        Tank tank = new Tank();
-        tank.goForward(10);
-        tank.printPosition();
-        tank.turnRight();
-        tank.goForward(50);
-        tank.printPosition();
-        tank.turnLeft();
-        tank.goBackward(100);
-        tank.printPosition();
+        // At (0;0) fuel=100
+        Tank justTank = new Tank();
+        // At (10;20) fuel=100
+        Tank anywareTank = new Tank(10, 10);
+        // At (20;30) fuel=200
+        Tank customTank = new Tank(20, 30, 200);
+        justTank.goForward(200);
+        justTank.printPosition();
+        anywareTank.goBackward(-200);
+        anywareTank.printPosition();
+        customTank.goForward(201);
+        customTank.printPosition();
 
 /*
 This fragment of code has to output
 
-The Tank is at 10 now.
-The Tank is at 60 now.
-The Tank is at -40 now.
+The Tank is at -100, 0 now.
+The Tank is at 120, -10 now.
+The Tank is at 170, 30 now.
 
 */
     }
--- Cat/src/Tank.java
+++ Cat/src/Tank.java
@@ -1,27 +1,38 @@
+import static java.lang.Math.abs;
+
 public class Tank {
     int x = 0;
     int y = 0;
     int dir;
+    int fuel = 100;
     
     public Tank() {
-        this(0, 0);
+        this(0, 0, 100);
+    }
+
+    public Tank(int x, int y, int fuel) {
+        this.x = x;
+        this.y = y;
+        this.fuel = fuel;
     }
 
     public Tank(int x, int y) {
-        this.x = x;
-        this.y = y;
+        this();
     }
 
     //0 - вправо, 1  - вниз, 2  - влево, 3 - вверх
     public void goForward(int x) {
-        if (dir==0) this.x += x;
-        else if (dir == 1) y+=x;
-        else if (dir == 2) this.x -= x;
-        else y -=x;
+        while (fuel!=0) {
+            if (dir==0) this.x += x;
+            else if (dir == 1) y+=x;
+            else if (dir == 2) this.x -= x;
+            else y -=x;
+            fuel-=abs(x);
+        }
     }
 
     public void goBackward(int x) {
-        goForward(-x);
+            goForward(-x);
     }
 
     public void printPosition() {
