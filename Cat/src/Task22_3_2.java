3968bfea90dad23a5c1512f3e0e507a661990dda
nastre3 <nastre70@gmail.com>
2021-11-14 14:06:21 +1000

    Added Task22_3_3-5

--- Cat/src/Task22_3_2.java
+++ Cat/src/Task22_3_2.java
@@ -4,7 +4,10 @@ import java.util.regex.Pattern;
 public class Task22_3_2 {
     public static void main(String[] args) {
         String text = "aba aba a!a abba adca abea";
-        Pattern pattern = Pattern.compile("a.{2}a");
+        Pattern pattern = Pattern.compile("a.{2}a"); // a..a
+        //22_3_3 - (ab)+
+        //22_3_4 - 20\d{2}
+        //22_3_5 - s\w{5,}s or \bs[a-z]*?s\b
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             int start = matcher.start();
