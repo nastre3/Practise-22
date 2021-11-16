9a0633f08c27be9636bd6f8565ddb49c18141c7e
nastre3 <nastre70@gmail.com>
2021-11-16 19:18:23 +1000

    Added in RegularExpression checking laugh

--- Cat/src/RegularExpression.java
+++ Cat/src/RegularExpression.java
@@ -7,5 +7,10 @@ public class RegularExpression {
         Matcher matcher = pattern.matcher("nastre70@gmail.com");
         boolean matches = matcher.matches();
         System.out.println(matches);
+
+        Pattern pattern2 = Pattern.compile("[Хх]([аоие])х?(?:\\1х?)*|[Аа]х?(?:ах?)+");
+        Matcher matcher2 = pattern2.matcher("Ахахаах");
+        boolean matches2 = matcher2.matches();
+        System.out.println(matches2);
     }
 }
